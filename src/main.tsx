6a6645e332880b508c6c00320cdabdbe38db0975
Myo Thant Naing <thant8642@gmail.com>
2025-11-20 15:13:13 +0630

    update ux

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Quiz from "./Quiz.tsx";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from "./Home.tsx";
 import Layout from "./Layout.tsx";
@@ -12,8 +12,9 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, Component: Home },
-      { path: "quiz", Component: Quiz },
+      { index: true, element: <Home /> },
+      { path: "quiz", element: <Quiz /> },
+      { path: "*", element: <Navigate to="/" replace /> },
     ],
   },
 ]);
